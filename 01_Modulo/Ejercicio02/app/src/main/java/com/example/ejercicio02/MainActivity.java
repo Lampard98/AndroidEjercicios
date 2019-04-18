54b3fff69c1c272a2a8ac61737eb039ec3b5cf02
Lampard98 <a.eperez@itstequila.edu.mx>
2019-04-17 22:52:57 -0500

    E2
    Suma

--- 01_Modulo/Ejercicio02/app/src/main/java/com/example/ejercicio02/MainActivity.java
+++ 01_Modulo/Ejercicio02/app/src/main/java/com/example/ejercicio02/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.ejercicio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtNum1) EditText txtNumero1;
+    @BindView(R.id.txtNum2) EditText txtNumero2;
+    @BindView(R.id.rbtnSuma) RadioButton rSuma;
+    @BindView(R.id.rbtnResta) RadioButton rResta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnCalcular) public void hacerCalculo(View v){
+
+        if(rResta.isChecked()==true){
+            float r=Float.parseFloat(txtNumero1.getText().toString())+
+                    Float.parseFloat(txtNumero2.getText().toString());
+            Log.v("Mensaje", "El resultado es: "+r);
+            Toast.makeText(this,"Presioné e hice suma",Toast.LENGTH_LONG).show();
+        }else if(rSuma.isChecked()==true){
+            float r=Float.parseFloat(txtNumero1.getText().toString())-
+                    Float.parseFloat(txtNumero2.getText().toString());
+            Log.v("Mensaje", "El resultado es: "+r);
+            Toast.makeText(this,"Presioné e hice resta",Toast.LENGTH_LONG).show();
+        }
     }
 }
